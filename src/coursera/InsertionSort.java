70a78d5b4dd83487fe5e52972669a1a1be7608e2
SahaiAbishek <sahaiabhi@gmail.com>
2017-10-10 12:44:55 -0400

    insertion sort

---
+++ src/coursera/InsertionSort.java
@@ -0,0 +1,27 @@
+package coursera;
+
+public class InsertionSort {
+
+	public static void sort(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (less(a[j], a[j - 1])) {
+					exch(a, j, j - 1);
+				} else {
+					break;
+				}
+			}
+		}
+	}
+
+	public static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+
+	public static void exch(Comparable[] a, int i, int j) {
+		Comparable temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+}
