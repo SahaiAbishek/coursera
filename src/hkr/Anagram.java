8ad87c8c1c991b30a5c8bcc038abfde067af8777
SahaiAbishek <sahaiabhi@gmail.com>
2017-11-16 12:05:13 -0500

    anagrams correct solution

--- src/hkr/Anagram.java
+++ src/hkr/Anagram.java
@@ -3,21 +3,34 @@ package hkr;
 public class Anagram {
 
 	public static int numberNeeded(String first, String second) {
-		
-		int count = 0;
+
+		int arr[] = new int[26];
+
 		for (int i = 0; i < first.length(); i++) {
-			String str = first.charAt(i)+"";
-			if(second.contains(str)){
-				count+=2;
-				second = second.replace(str, "");
-			}
-			
+			char c = first.charAt(i);
+			int pos = c - 'a';
+			int num = arr[pos];
+			arr[pos] = ++num;
 		}
-		return first.length()+second.length() - count;
+
+		for (int i = 0; i < second.length(); i++) {
+			char c = second.charAt(i);
+			int pos = c - 'a';
+			int num = arr[pos];
+			arr[pos] = --num;
+		}
+
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + Math.abs(arr[i]);
+		}
+
+		return sum;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(numberNeeded("bacdc", "dcbad"));
+		System.out.println(numberNeeded("abcd", "cab"));
+
 	}
 
 }
