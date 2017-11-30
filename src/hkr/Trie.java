ffaa94314b4a01ac86e213fb0b0785b76d48c836
SahaiAbishek <sahaiabhi@gmail.com>
2017-11-30 14:18:26 -0500

    trie

--- src/hkr/Trie.java
+++ src/hkr/Trie.java
@@ -1,50 +1,66 @@
 package hkr;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Scanner;
 
 public class Trie {
 
-	private class Node {
-		private Map<Character, Node> children = new HashMap<>();
-		private boolean isCompleteWord = false;
+	private static class Node {
+		private Node[] arr = new Node[26];
+		// private boolean isWord = false;
+		private int count = 0;
 	}
 
-	private Node node;
+	private static Node root = new Node();
 
-	public void add(Character c) {
-		if (node == null) {
-			node = new Node();
-			node.children.put(c, node);
-		} else {
-			node.children.put(c, node);
+	public static void add(String word) {
+
+		Node node = root;
+		for (int i = 0; i < word.length(); i++) {
+			Character currentChar = word.charAt(i);
+			int index = currentChar - 'a';
+			if (node.arr[index] == null) {
+				Node temp = new Node();
+				temp.count = 1;
+				node.arr[index] = temp;
+				node = temp;
+			} else {
+				
+				node = node.arr[index];
+				node.count++;
+			}
+			// node.isWord = true;
 		}
 	}
 
-	@Override
-	public String toString() {
-		Map<Character, Node> map = node.children;
-		Iterator<Entry<Character, Node>> itr = map.entrySet().iterator();
-		while(itr.hasNext()){
-			Entry<Character, Node> entry = itr.next();
-			System.out.println(entry.getKey());
-			System.out.println(entry.getValue());
+	public static int find(String word) {
+		int count = 0;
+		Node node = root;
+		for (int i = 0; i < word.length(); i++) {
+			Character currentChar = word.charAt(i);
+			int index = currentChar - 'a';
+			if (node.arr[index] == null) {
+				return 0;
+			} else {
+				node = node.arr[index];
+			}
+			count = node.count;
 		}
-		
-		return super.toString();
+		return count;
 	}
 
 	public static void main(String[] args) {
-		Trie trie = new Trie();
-		String str = "hack";
-		for (int i = 0; i < str.length(); i++) {
-			trie.add(str.charAt(i));
-		}
-
-		System.out.println(trie);
-
+		
+		 Scanner in = new Scanner(System.in);
+	        int n = in.nextInt();
+	        for(int a0 = 0; a0 < n; a0++){
+	            String op = in.next();
+	            String contact = in.next();
+	            if(op.equals("add")){
+		            	add(contact);
+		            }else if(op.equals("find")){
+		            	System.out.println(find(contact));
+		            }
+	        }
 	}
 
 }
