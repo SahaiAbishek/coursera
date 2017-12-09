c71d3e362b3cc30cee3059cb32be6177d9bce695
SahaiAbishek <sahaiabhi@gmail.com>
2017-12-09 15:12:20 -0500

    Update KdTree.java

--- src/KdTree.java
+++ src/KdTree.java
@@ -1,5 +1,7 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
 
@@ -110,27 +112,133 @@ public class KdTree {
 
 	}
 
+	private boolean contains(Node root, Point2D p, boolean vert) {
+		if (root == null) {
+			return false;
+		}
+		if (root.point.equals(p)) {
+			return true;
+		}
+
+		int cmp = 0;
+		if (vert) {
+			cmp = Point2D.X_ORDER.compare(root.point, p);
+		} else {
+			cmp = Point2D.Y_ORDER.compare(root.point, p);
+		}
+		if (cmp > 0)
+			return contains(root.left, p, !vert);
+		else
+			return contains(root.right, p, !vert);
+	}
+
 	// does the set contain point p?
 	public boolean contains(Point2D p) {
 
-		return false;
+		return contains(root,p,true);
 	}
 
-	// draw all points to standard draw
-	public void draw() {
+	 private void draw(Node x, boolean vert) {
+	        if (x.left != null)     draw(x.left, !vert);
+	        if (x.right != null)    draw(x.right, !vert);
 
-	}
+	        // draw the point first
+	        StdDraw.setPenColor(StdDraw.BLACK);
+	            StdDraw.setPenRadius(.01);
+	            StdDraw.point(x.point.x(), x.point.y());
 
-	// all points that are inside the rectangle (or on the boundary)
-	public Iterable<Point2D> range(RectHV rect) {
+	            // draw the line
+	            double xmin, ymin, xmax, ymax;
+	            if (vert) {
+	                StdDraw.setPenColor(StdDraw.RED);
+	                xmin = x.point.x();
+	                xmax = x.point.x();
+	                ymin = x.rect.ymin();
+	                ymax = x.rect.ymax();
+	            } else {
+	                StdDraw.setPenColor(StdDraw.BLUE);
+	                ymin = x.point.y();
+	                ymax = x.point.y();
+	                xmin = x.rect.xmin();
+	                xmax = x.rect.xmax();
+	            }
+	            StdDraw.setPenRadius();
+	            StdDraw.line(xmin, ymin, xmax, ymax);
+	    }
 
-		return null;
-	}
+	    // draw all of the points to standard draw
+	    public void draw() {
+	        // draw the rectangle
+	        StdDraw.rectangle(0.5, 0.5, 0.5, 0.5);
+	        if (isEmpty()) return;
+	        draw(root, true);
+	    }
 
-	// a nearest neighbor in the set to point p; null if the set is empty
-	public Point2D nearest(Point2D p) {
-		return null;
-	}
+
+	private void range(Node x, RectHV rect, Queue<Point2D> q) {
+        if (x == null)
+            return;
+        if (rect.contains(x.point))
+            q.enqueue(x.point);
+        if (x.left != null && rect.intersects(x.left.rect))
+            range(x.left, rect, q);
+        if (x.right != null && rect.intersects(x.right.rect))
+            range(x.right, rect, q);
+    }
+
+    // all points in the set that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        Queue<Point2D> q = new Queue<Point2D>();
+        range(root, rect, q);
+        return q;
+    }
+	
+
+    private Point2D nearest(Node x, Point2D p, Point2D mp, boolean vert) {
+        Point2D min = mp;
+
+        if (x == null)    return min;
+        if (p.distanceSquaredTo(x.point) < p.distanceSquaredTo(min))
+            min = x.point;
+
+        // choose the side that contains the query point first
+        if (vert) {
+            if (x.point.x() < p.x()) {
+                min = nearest(x.right, p, min, !vert);
+                if (x.left != null
+                        && (min.distanceSquaredTo(p)
+                            > x.left.rect.distanceSquaredTo(p)))
+                    min = nearest(x.left, p, min, !vert);
+            } else {
+                min = nearest(x.left, p, min, !vert);
+                if (x.right != null
+                        && (min.distanceSquaredTo(p)
+                         > x.right.rect.distanceSquaredTo(p)))
+                    min = nearest(x.right, p, min, !vert);
+            }
+        } else {
+            if (x.point.y() < p.y()) {
+                min = nearest(x.right, p, min, !vert);
+                if (x.left != null
+                        && (min.distanceSquaredTo(p)
+                            > x.left.rect.distanceSquaredTo(p)))
+                    min = nearest(x.left, p, min, !vert);
+            } else {
+                min = nearest(x.left, p, min, !vert);
+                if (x.right != null
+                        && (min.distanceSquaredTo(p)
+                            > x.right.rect.distanceSquaredTo(p)))
+                    min = nearest(x.right, p, min, !vert);
+            }
+        }
+        return min;
+    }
+
+    // a nearest neighbor in the set to p: null if set is empty
+    public Point2D nearest(Point2D p) {
+        if (isEmpty()) return null;
+        return nearest(root, p, root.point, true);
+    }
 
 	// unit testing of the methods (optional)
 	public static void main(String[] args) {
