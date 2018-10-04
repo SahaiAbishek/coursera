a595d221ac7b1d8cb7a2eae1181b9c3b48e08375
SahaiAbishek <sahaiabhi@gmail.com>
2018-10-04 19:53:23 -0400

    correct solution for hourglass

--- src/hkr/HourGlassProblem.java
+++ src/hkr/HourGlassProblem.java
@@ -6,18 +6,18 @@ public class HourGlassProblem {
 	static int hourglassSum(int[][] arr) {
 
 		int max = Integer.MIN_VALUE;
-		
+		int sum = 0;
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 6; j++) {
-				int sum = 0;
 				if (i + 2 < 6 && j + 2 < 6) {
-					sum += arr[i][j]+arr[i][j+1]+arr[i][j+2]
+					sum = arr[i][j]+arr[i][j+1]+arr[i][j+2]
 							+arr[i+1][j+1]+
 						arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+					if(sum > max){
+						max = sum;
+					}
 				}
-				if(sum > max){
-					max = sum;
-				}
+				
 			}
 		}
 		return max;
